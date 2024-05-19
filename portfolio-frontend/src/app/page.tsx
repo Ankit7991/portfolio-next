2ada9875e29c7022a0577b4dd86d965631f45cea
Ankit Maurya <57867346+Ankit7991@users.noreply.github.com>
2024-05-19 18:17:35 +0530

    manage random positions of tiles in empty grid

--- portfolio-frontend/src/app/page.tsx
+++ portfolio-frontend/src/app/page.tsx
@@ -1,31 +1,63 @@
+'use client';
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
 import SideBarIntro from "@/components/pages/sidebar-intro.pages";
 import Block from "@/components/micro/block/block";
 import LogoFindAnkit from "@/components/sections/logo/m.findankit";
+import { useEffect, useMemo, useState } from "react";
+import { useRandomPositions } from "@/hooks/get-random-positions.hook";
+
+const gte = (a: number, ref: number) => typeof(ref) === 'number' ? a >= ref : false;
+const lte = (b: number, ref: number) => typeof(ref) === 'number' ? b <= ref : false;
+const btwn = (max: number, min: number, ref: number) => gte(max, ref) && lte(min, ref);
+
 
 export default function Home() {
+
+	const {freeSpaces, randomSpaces} = useRandomPositions([{text: 'ankit'}, {text: 'node'}], [
+    [3, 13, 2, 9],
+    [1, 13, 1, 2],
+    [5, 6, 10, 11],
+  ]);
+
   return (
-		<main className="fluid">
+    <main className="fluid">
       <SideBarIntro label="INTRO">
         <Block rowStart={3} rowEnd={13} colStart={2} colEnd={9}>
-					<p>Hi, My name is</p>
-					<h1>Ankit</h1>
+          <div className={styles.info}>
+            <p>Hi, My name is</p>
+            <h1 className={styles.name}>Ankit</h1>
 
-					<div>
-						<p>
-							I am a full-stack web developer with 4+ years of experience in technologies like Node, React, and MySQL. 
-						</p>
-						<p>
-							I’ve built and deployed projects in various domains like CRM, Fitness, Surveys, and Podcasts. 
-						</p>
-						<p>
-	I'm a lifelong learner, constantly keeping pace with the latest industry trends. My passion lies in crafting robust and user-friendly web applications that deliver exceptional value. Throughout my career, I've honed my skills in both frontend and backend development through diverse projects. I thrive in collaborative environments that value innovation and teamwork.
-
-						</p>
-					</div>
+            <div>
+              <p>
+                <span className={styles.mid}>I am a full-stack web developer.</span>
+              </p>
+              <p>
+                I’ve built and deployed projects in various domains like CRM,
+                Fitness, Surveys, and Podcasts.
+              </p>
+              <p>
+                I'm a lifelong learner, constantly keeping pace with the latest
+                industry trends. My passion lies in crafting robust and
+                user-friendly web applications that deliver exceptional value.
+                Throughout my career, I've honed my skills in both frontend and
+                backend development through diverse projects. I thrive in
+                collaborative environments that value innovation and teamwork.
+              </p>
+            </div>
+          </div>
         </Block>
+				{
+					randomSpaces?.map(el => {
+						
+						return <>
+							<Block rowStart={el.rowLine} colStart={el.colLine}>
+								{el.info?.text}
+							</Block>
+						</>
+					})
+				}
       </SideBarIntro>
-		</main>
+    </main>
   );
 }
---
+++ portfolio-frontend/src/hooks/get-random-positions.hook.tsx
@@ -0,0 +1,85 @@
+import { useEffect, useState } from "react";
+
+export interface IRandomPositionElement {
+  text: string;
+  url?: string;
+}
+export type IGridOccupiedArea = [
+  rowStart: number,
+  rowEnd: number,
+  colStart: number,
+  colEnd: number
+];
+export interface IGridLocations {
+  rowLine: number;
+  colLine: number;
+	info?: IRandomPositionElement;
+}
+
+
+export function useRandomPositions(
+  elements: IRandomPositionElement[],
+  usedSpaces: IGridOccupiedArea[]
+) {
+  const [freeSpaces, setFreeSpaces] = useState<IGridLocations[]>([]);
+	const [randomSpaces, setRandomSpaces] = useState<IGridLocations[]>();
+
+  useEffect(() => {
+    function calcSpace(usedSpaces: IGridOccupiedArea[], total = 1, r = null, c = null) {
+      let n = 12; // 12 Grids -> 13 lines
+      let rowLines = r || new Array(n).fill("").map((el, i) => i + 1);
+      let colLines = c || [...rowLines];
+      let grid: IGridLocations[] = [];
+
+      // check if any rowLine & colLine are free or not .. row/colLine is concept of grid css
+      function areaUsed([rowLine, colLine]: number[], usedSpaces: IGridOccupiedArea[]) {
+        let used = false;
+        usedSpaces.map((space) => {
+          if (used) return;
+          const [rowStart, rowEnd, colStart, colEnd] = space;
+          used =
+            colLine >= colStart &&
+            colLine < colEnd &&
+            rowLine >= rowStart &&
+            rowLine < rowEnd;
+        });
+        return used;
+      }
+      rowLines.map((rl) => {
+        colLines.map((cl) => {
+          let used = areaUsed([rl, cl], usedSpaces);
+          if (!used) grid.push({ rowLine: rl, colLine: cl });
+        });
+      });
+
+      setFreeSpaces(grid);
+    }
+		
+		
+    calcSpace(usedSpaces);
+  }, []);
+	
+	useEffect(() => {
+		
+				const getRandomElements = (arr: IGridLocations[], count: number) => {
+					const shuffled = arr.slice();
+					for (let i = arr.length - 1; i > 0; i--) {
+						const j = Math.floor(Math.random() * (i + 1));
+						[shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+					}
+					return shuffled.slice(0, count);
+				};
+
+			const randomLocations = (getRandomElements(freeSpaces, elements.length)).map((el, i: number) => {
+				el.info = elements[i];
+				return el;
+			});
+
+			setRandomSpaces(randomLocations);
+	}, [freeSpaces])
+
+  return {
+    freeSpaces,
+		randomSpaces
+  };
+}
